047fef62589689e878da22ad6b46de21e537142e
Luiz Felipe Machado da Silva <lfmachadodasilva@gmail.com>
2021-03-17 15:17:57 +0000

    first unit test

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 
 @Controller('forecast')
 export class ForecastController {
-  @Get('')
+  @Get()
   public getForecastForgeLoggedUser(_: Request, res: Response): void {
     res.send([
       {
